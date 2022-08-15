d30cd86c39388f17c97042b0d92d7f30f7feee65
benhid <antoniobenitezhid@gmail.com>
2022-08-16 01:18:31 +0200

    added pino logger

--- index.js
+++ index.js
@@ -1,41 +1,38 @@
 const http = require("http");
 const fs = require('fs');
-const io = require('socket.io')();
+const SocketIO = require('socket.io');
 const {randomUUID} = require('crypto');
 
-const server = http.createServer(function (req, res) {
+const pino = require('pino')();
+const pinoHttp = require('pino-http')({logger: pino});
+
+// prepares files to be served
+const html = fs.readFileSync(__dirname + '/dist/room.html', 'utf8');
+const css = fs.readFileSync(__dirname + '/dist/style.css', 'utf8');
+
+const httpServer = http.createServer((req, res) => {
+    pinoHttp(req, res);
     if (req.url === '/') {
         res.writeHead(200, {"Content-Type": "text/plain"});
-        res.write(`https://ghostie.link/${randomUUID()}`, "utf8");
-        res.end();
+        res.end(`https://ghostie.link/${randomUUID()}`);
     } else if (req.url === '/style.css') {
-        fs.readFile("./dist/style.css", function (err, css) {
-            if (err) throw err;
-            res.writeHead(200, {"Content-Type": "text/css"});
-            res.write(css, "utf8");
-            res.end();
-        });
+        res.writeHead(200, {"Content-Type": "text/css", 'Content-Length': Buffer.byteLength(css)});
+        res.end(css);
     } else if (!(req.url.split(/\/(?=.)/).length === 2)) {
         // not a valid URL (ie. contains subdirectories)
         res.writeHead(404, {"Content-Type": "text/plain"});
-        res.write("404 Not Found", "utf8");
-        res.end();
+        res.end("404 Not Found");
     } else {
         if (req.method === 'GET') {
-            fs.readFile("./dist/room.html", function (err, html) {
-                if (err) throw err;
-                res.writeHead(200, {"Content-Type": "text/html"});
-                res.write(html, "utf8");
-                res.end();
-            });
+            res.writeHead(200, {"Content-Type": "text/html", 'Content-Length': Buffer.byteLength(html)});
+            res.end(html);
         } else {
-            // TODO - limit size of POST body
-            let body = '';
+            let event = '';
             req.on('data', function (data) {
-                body += data;
+                event += data;
             });
             req.on('end', function () {
-                io.to(`room${req.url}`).emit(body)
+                io.sockets.in(req.url).emit("event", event);
                 res.writeHead(200);
                 res.end();
             });
@@ -43,27 +40,24 @@ const server = http.createServer(function (req, res) {
     }
 });
 
-console.log('listening on http://localhost:3000');
-
-server.listen(3000);
-
 // attach Socket.io to our HTTP server
-io.listen(server);
+const io = SocketIO(httpServer);
 
 // handle incoming connections from clients
 io.on("connection", (socket) => {
-    console.log(`msg=connected socket_id=${socket.id}`);
+    pino.debug({"event": "connected", "msg": "new connection", "socket_id": socket.id});
     // once a client has connected, we expect to get a ping from them saying what room they want to join
-    socket.on('join', function (room) {
-        console.log(`msg=joined socket_id=${socket.id} room=room${room}`);
-        socket.join(`room${room}`);
+    socket.on('join', async (room) => {
+        pino.debug({"event": "join", "msg": "joined", "socket_id": socket.id, "room": room});
+        socket.join(room);
+        //socket.emit("event", history);
     });
-    socket.on('disconnected', function () {
-        console.log(`msg=disconnected socket_id=${socket.id}`);
+    socket.on('disconnected', async (room) => {
+        pino.debug({"event": "disconnected", "msg": "socket disconnected", "socket_id": socket.id, "room": room});
     });
-    // let server timeout 10secs
-    //setTimeout(() => {
-    //    socket.disconnect(true);
-    //    console.log(socket.connected);
-    //}, 10000);
+});
+
+// starts up server
+httpServer.listen(3000, () => {
+    pino.info('listening on http://localhost:3000');
 });
